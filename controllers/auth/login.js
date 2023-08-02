9ecc433f7495bf93f165f30d47e8c41e561167f5
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-08-02 21:48:40 +0300

    Login

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,7 +1,36 @@
+const { User } = require('../../models');
+const { authSchema } = require('../../schemas');
+const { HttpError } = require('../../helpers');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const { SECRET_KEY } = process.env;
 
 const login = async (req, res, next) => {
+    const { value, error } = authSchema.loginSchema.validate(req.body);
+    if (error) {
+        throw HttpError(400, `Missing or invalid required ${error.details[0].context.label} field`);
+    };
 
+    const { email, password } = value;
+    const user = await User.findOne({ email });
+    if (!user) throw HttpError(401, 'Email is wrong');
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) throw HttpError(401, 'Password is wrong');
+
+    const accessToken = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: '30m' });
+    await User.findOneAndUpdate({email}, {accessToken});
+    res.json({
+        token: accessToken,
+        user: {
+            name: user.name,
+            email: user.email,
+            theme: user.theme,
+            avatarURL: user.avatarURL,
+            boards: user.boards,
+        },
+    });
 };
 
 module.exports = login;
