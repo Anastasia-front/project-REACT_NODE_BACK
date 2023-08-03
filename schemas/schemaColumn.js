049cc244c7e0ff5a028148f2f63738f4283c1b7e
Maksym Ostapchuk <gadget.add@gmail.com>
2023-08-03 16:18:08 +0300

    add joi schema

--- schemas/schemaColumn.js
+++ schemas/schemaColumn.js
@@ -1,11 +1,13 @@
 const Joi = require("joi");
+Joi.objectId = require('joi-objectid')(Joi);
 
 const addColumnSchema = Joi.object({
-  //
+  title: Joi.string().required(),
+  board: Joi.objectId().required(),
 });
 
 const editColumnSchema = Joi.object({
-  //
+  title: Joi.string().required(),
 });
 
 const columnSchemas = { addColumnSchema, editColumnSchema };
