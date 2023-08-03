8a8aa1318b9ab5e88f2faee183c1fac15641e568
Maksym Ostapchuk <gadget.add@gmail.com>
2023-08-03 22:37:53 +0300

    add schema

--- schemas/schemaTask.js
+++ schemas/schemaTask.js
@@ -1,11 +1,23 @@
 const Joi = require("joi");
+Joi.objectId = require('joi-objectid')(Joi);
 
 const addTaskSchema = Joi.object({
-  //
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  priority: Joi.string().valid('without', 'low', 'medium', 'high').messages({
+    'any.only': "Only 'without', 'low', 'medium', 'high' values are allowed",
+  }),
+  deadline: Joi.string().regex(/^\d{8}$/).allow(null),
+  column: Joi.objectId().required(),
 });
 
 const editTaskSchema = Joi.object({
-  //
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  priority: Joi.string().valid('without', 'low', 'medium', 'high').messages({
+    'any.only': "Only 'without', 'low', 'medium', 'high' values are allowed",
+  }),
+  deadline: Joi.string().regex(/^\d{8}$/).allow(null),
 });
 
 const taskSchemas = { addTaskSchema, editTaskSchema };
