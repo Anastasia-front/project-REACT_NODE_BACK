0d078a808bec96ebafdeffd6ce47e5a3caefd9ae
Maksym Ostapchuk <gadget.add@gmail.com>
2023-08-03 13:57:10 +0300

    add board joi schema

--- schemas/schemaBoard.js
+++ schemas/schemaBoard.js
@@ -1,7 +1,19 @@
 const Joi = require("joi");
+Joi.objectId = require('joi-objectid')(Joi);
 
 const addBoardSchema = Joi.object({
-  //
+  title: Joi.string().required(),
+  icon: Joi.string().valid(
+    'icon-colors',
+    'icon-container',
+    'icon-hexagon',
+    'icon-lightning',
+    'icon-loading',
+    'icon-project',
+    'icon-star',
+    'icon-puzzle-piece',
+  ),
+  background: Joi.objectId().allow(null),  
 });
 
 const editBoardSchema = Joi.object({
