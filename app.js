fa0e63c241df492fce21afb72746cb7bacdf95ee
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-08-04 17:56:21 +0300

    creates swagger for auth router

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerDocument = require("./swagger.json");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const { routerAuth } = require("./routes/api");
 // const routerBoards = require("./routes/api");
@@ -18,7 +18,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/users", routerAuth);
 // app.use("/api/boards", routerBoards);
 // app.use("/api/columns", routerColumns);
