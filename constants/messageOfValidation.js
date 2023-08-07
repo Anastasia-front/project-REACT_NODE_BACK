3dd24b9ee2ec5d96da624ecaea97559b0cf1313c
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-08-07 21:01:16 +0300

    refactor: validation of password changed

--- constants/messageOfValidation.js
+++ constants/messageOfValidation.js
@@ -5,7 +5,7 @@ const fieldRequired = (field) => {
 const emailInvalid =
   "Invalid email field. It must contain only Latin letters and meet all requirements for mail; may include numbers, letters in different case";
 const passwordInvalid =
-  "Invalid password field. It must contain only Latin letters, at least one uppercase, lowercase letter and a number; does not contain a space";
+  "Invalid password field. It must contain only Latin letters;  may contain numbers, signs, letters in different case; does not contain a space";
 const nameInvalid =
   "Invalid name field. It must contain only Latin letters; may contain space, numbers, signs, letters in different case";
 
