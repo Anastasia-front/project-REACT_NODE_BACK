a7442aa41d0ef67b43a407acfe114f4ed6aecad2
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-08-09 18:04:08 +0300

    delete boards from select at response of /api/users/update

--- controllers/auth/updateUser.js
+++ controllers/auth/updateUser.js
@@ -14,7 +14,7 @@ const updateUser = async (req, res, next) => {
   const { name = oldName, email, password } = value;
   const updatedUser = {
     name,
-  }
+  };
 
   if (req.file) {
     updatedUser.avatarURL = await uploadToCloud(req);
@@ -24,22 +24,21 @@ const updateUser = async (req, res, next) => {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
     updatedUser.password = hashedPassword;
-    updatedUser.accessToken = '';
-    res.status(204)
+    updatedUser.accessToken = "";
+    res.status(204);
   }
 
   if (email && email !== oldEmail) {
     updatedUser.email = email;
-    updatedUser.accessToken = '';
-    res.status(204)
+    updatedUser.accessToken = "";
+    res.status(204);
   }
 
   const result = await User.findByIdAndUpdate(_id, updatedUser, {
     new: true,
-    select: "name email theme avatarURL boards -_id",
+    select: "name email theme avatarURL -_id",
   });
   res.json(result);
 };
-
 
 module.exports = updateUser;
