b23e3e670297b4f2154d26df7b08c47088b6713b
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-08-10 22:16:08 +0300

    register and login

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -3,9 +3,11 @@ const queryString = require('query-string');
 const { GOOGLE_CLIENT_ID, HOSTING_URL, BASE_URL } = process.env;
 
 const googleAuth = (req, res) => {
+    const serverHOST = req.get("host") === "localhost:3007" ? BASE_URL : HOSTING_URL;
+
     const strigifiedParams = queryString.stringify({
         client_id: GOOGLE_CLIENT_ID,
-        redirect_uri: `${BASE_URL}/api/users/google-redirect`,
+        redirect_uri: `${serverHOST}/api/users/google-redirect`,
         scope: [
             "https://www.googleapis.com/auth/userinfo.email",
             "https://www.googleapis.com/auth/userinfo.profile",
@@ -15,7 +17,7 @@ const googleAuth = (req, res) => {
         prompt: "consent",
     });
 
-    return res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?${strigifiedParams}`)
+    return res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?${strigifiedParams}`);
 };
 
 module.exports = googleAuth;
--- controllers/auth/googleRedirect.js
+++ controllers/auth/googleRedirect.js
@@ -1,5 +1,6 @@
 const queryString = require('query-string');
 const axios = require('axios');
+const { User } = require("../../models");
 
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, HOSTING_URL, BASE_URL } = process.env;
 
@@ -8,6 +9,7 @@ const googleRedirect = async (req, res) => {
     const urlObj = new URL(fullUrl);
     const urlParams = queryString.parse(urlObj.search);
     const code = urlParams.code;
+    const serverHOST = req.get("host") === "localhost:3007" ? BASE_URL : HOSTING_URL;
 
     const tokenData = await axios({
         url: "https://oauth2.googleapis.com/token",
@@ -15,13 +17,13 @@ const googleRedirect = async (req, res) => {
         data: {
             client_id: GOOGLE_CLIENT_ID,
             client_secret: GOOGLE_CLIENT_SECRET,
-            redirect_uri: `${BASE_URL}/api/users/google-redirect`,
+            redirect_uri: `${serverHOST}/api/users/google-redirect`,
             grant_type: "authorization_code",
             code,
         },
     });
 
-    const userData = await axios({
+    const {data: {id, email, name}} = await axios({
         url: "https://www.googleapis.com/oauth2/v2/userinfo",
         method: "get",
         headers: {
@@ -29,7 +31,28 @@ const googleRedirect = async (req, res) => {
         },
     });
 
-    res.json(userData.data);
+    const user = await User.findOne({ email });
+    if (!user) {
+        const result = await User.create({
+            name,
+            email,
+            password: id,
+        });
+
+        const accessToken = result.signToken();
+        await User.findOneAndUpdate({ email }, { accessToken });
+
+        return res.redirect(`https://tkachenko01001.github.io/project-REACT_NODE/auth/login/${accessToken}`);
+    };
+
+    if (user) {
+        const accessToken = user.signToken();
+        await User.findOneAndUpdate({ email }, { accessToken });
+        return res.redirect(`https://tkachenko01001.github.io/project-REACT_NODE/auth/login/${accessToken}`);
+    }
+
+
+    res.json({rez: 'уже есть такой'});
 };
 
 module.exports = googleRedirect
