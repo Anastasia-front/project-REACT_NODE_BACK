c0763da61de92f6a161b2782dfcbdb4a6164a680
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-08-11 12:23:33 +0300

    refactor: add .json() to status(204)

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -3,7 +3,7 @@ const { User } = require("../../models");
 const logout = async (req, res, next) => {
   const { user } = req;
   await User.findOneAndUpdate({ _id: user._id }, { accessToken: "" });
-  res.status(204);
+  res.status(204).json();
 };
 
 module.exports = logout;
