ada4d494c569467567383ba3dd9f8b91938091bd
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-08-12 23:28:16 +0300

    refactor: change regExp of deadline

--- constants/regExp.js
+++ constants/regExp.js
@@ -1,7 +1,7 @@
 const email = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/;
 const password = /^[a-zA-Z0-9\-!@#$%^&*()_+,.:;'"?/]+$/;
 const name = /^[a-zA-Z0-9 !@#$%^&*()_+,.:;'"?/-]+$/;
-const deadline = /^\d{8}$/;
+const deadline = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
 
 module.exports = {
   email,
