a2f4806d3829e2fa50ecad914ee5a50d886e27cc
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-08-14 22:13:19 +0300

    refactor: update swagger

--- controllers/columns/updateColumn.js
+++ controllers/columns/updateColumn.js
@@ -1,19 +1,19 @@
-const { Column } = require("../../models")
+const { Column } = require("../../models");
 const { columnSchemas } = require("../../schemas");
-const { HttpError, BadRequestError } = require('../../helpers')
+const { HttpError, BadRequestError } = require("../../helpers");
 
 const updateColumn = async (req, res) => {
-    const { value, error } = columnSchemas.updateColumnSchema.validate(req.body, {
-        abortEarly: false,
-    });
-    if (error) BadRequestError(error);
-    const { id } = req.params;
-    const { title } = value;
-    const result = await Column.findByIdAndUpdate(id, { title }, { new: true });
-    if (!result) {
-        throw HttpError(404, `Column ${id} not found`);
-    }
-    res.status(201).json(result);
+  const { value, error } = columnSchemas.updateColumnSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (error) BadRequestError(error);
+  const { id } = req.params;
+  const { title } = value;
+  const result = await Column.findByIdAndUpdate(id, { title }, { new: true });
+  if (!result) {
+    throw HttpError(404, `Column ${id} not found`);
+  }
+  res.status(200).json(result);
 };
 
 module.exports = updateColumn;
